2ff457ea86712bce3ebf435ae6bb5f7bb8a4ce22
hparizani <henry_lsg@hotmail.com>
2024-05-19 21:25:44 -0300

    Adiciona funcionalidade

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        String agencia, nomeCliente;
+        double saldo = 1547.25;
+
+        System.out.println("Por favor, digite o número da agência: ");
+        agencia = scanner.next();
+        System.out.println("Por favor, digite o número da conta: ");
+        numero = scanner.nextInt();
+        System.out.println("Por favor, digite o seu nome: ");
+        nomeCliente = scanner.next();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + numero + " e seu saldo R$" + saldo + " já está disponível para saque.");
+
+        
+    }
 }
